2e06a6fa917bc968aa3e79eecdfc26ab387f33f5
Matt Provost <mattprovost6@gmail.com>
2023-07-31 09:17:45 -0700

    Merge pull request #1 from BSFishy/big_update
    Big update
    
    (cherry picked from commit 2430b82b5d62b8aed84bdbf661093e184321127b)

--- lib/sass_extract.dart
+++ lib/sass_extract.dart
@@ -1,14 +1,21 @@
+import 'package:path/path.dart' as p;
 import 'package:sass/sass.dart';
 import 'package:sass/src/ast/sass/statement/stylesheet.dart';
-import 'package:sass/src/importer/filesystem.dart';
+import 'package:sass/src/async_import_cache.dart';
+import 'package:sass/src/import_cache.dart';
+import 'src/io.dart';
+import 'src/async_visitor.dart';
 import 'src/visitor.dart';
 import 'src/value.dart';
 
-Map<String, Object?> extractVariablesFromString(String source, {String? path}) {
-  var stylesheet = Stylesheet.parseScss(source);
-  var visitor = EvaluateVisitor();
-  var module = visitor.run(FilesystemImporter(path ?? '.'), stylesheet);
-  var variables = module.variables;
+Map<String, Object?> extractVariablesFromString(String source, {String? path, Object? url, Syntax? syntax}) {
+  final stylesheet = Stylesheet.parse(source, syntax ?? Syntax.scss, url: url);
+  final importer = FilesystemImporter(path ?? '.');
+
+  final importCache = ImportCache(importers: [importer]);
+  final visitor = EvaluateVisitor(importCache: importCache);
+  final module = visitor.run(importer, stylesheet);
+  final variables = module.variables;
 
   Map<String, Object?> output = {};
   for (final key in variables.keys) {
@@ -16,4 +23,33 @@ Map<String, Object?> extractVariablesFromString(String source, {String? path}) {
   }
 
   return output;
+}
+
+Map<String, Object?> extractVariablesFromFile(String url, {String? path, Syntax? syntax}) {
+  final source = readFile(url);
+
+  return extractVariablesFromString(source, path: path ?? p.dirname(source), url: url, syntax: syntax);
+}
+
+Future<Map<String, Object?>> extractVariablesFromStringAsync(String source, {String? path, Object? url, Syntax? syntax}) async {
+  final stylesheet = Stylesheet.parse(source, syntax ?? Syntax.scss, url: url);
+  final importer = FilesystemImporter(path ?? '.');
+
+  final importCache = AsyncImportCache(importers: [importer]);
+  final visitor = AsyncEvaluateVisitor(importCache: importCache);
+  final module = await visitor.run(importer, stylesheet);
+  final variables = module.variables;
+
+  Map<String, Object?> output = {};
+  for (final key in variables.keys) {
+    output[key] = valueToObject(variables[key]);
+  }
+
+  return output;
+}
+
+Future<Map<String, Object?>> extractVariablesFromFileAsync(String url, {String? path, Syntax? syntax}) async {
+  final source = readFile(url);
+
+  return await extractVariablesFromStringAsync(source, path: path ?? p.dirname(source), url: url, syntax: syntax);
 }
